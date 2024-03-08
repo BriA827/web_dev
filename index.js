939fc23a29a03d41f5e4aa049cd4ee536571a55f
BriA827 <brigamer13@gmail.com>
2024-03-08 12:44:13 -0500

    commit

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ menuBtn.addEventListener("click", () => {
 xBtn.addEventListener("click", () => {
     linkDiv.style.display = "none"
 })
+
+// //dad jokes
+// const output = document.querySelector(".output")
+// const jokeButton = document.querySelector(".joke-button")
+
+// const url = 'https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'be1406aef5msh3f3e69275e1aa00p1707fcjsnfc291814c0b7',
+// 		'X-RapidAPI-Host': 'dad-jokes-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// async function getJoke(){
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         const joke = result[0].joke;
+//         output.textContent = joke
+//     } catch (error) {
+//         output.textContent = "Something went wrong"
+//     }
+// }
+
+// jokeButton.addEventListener("click", () => {
+//     getJoke()
+// })
